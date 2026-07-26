7d06f74af3123bbd50bdea062afeea917a6bb3e2
Jekath <>
2026-07-26 13:42:04 +0200

    Setting up Main component

---
+++ src/components/Main.vue
@@ -0,0 +1,94 @@
+<script setup>
+    import {ref} from 'vue'
+    const quote = ref("First, solve the problem. Then, write the code")
+    const author = ref("John Johnson")
+    const href = ref("https://en.wikipedia.org/wiki/Undocumented_feature")
+    const isBtnDisabled = ref(false)
+
+    quote.value = "It's a feature, not a bug."
+    author.value = "Unknown" 
+</script>
+
+<template>
+  <main>
+      <section>
+          <p>{{quote}}</p>
+          <a target="_blank" :href><span>{{author}}</span></a>
+      </section>
+      <section id="buttons">
+        <button :disabled="isBtnDisabled">Another!</button>
+        <button :disabled="isBtnDisabled">Share</button>
+    </section>
+  </main>
+</template>
+
+<style scoped>
+  main {
+    background: #CCD6D9;
+    max-width:90%;
+    border-radius:0 15px 0 15px;
+    padding:35px;
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+    gap:10px;
+  }
+
+  section {
+      display: flex;
+      width:100%;
+      flex-direction:column;
+      gap:10px;
+      margin-bottom:25px;
+  }
+
+  p {
+      font-weight:bold;
+      font-style: italic;
+      font-size:2rem;
+      text-align:left;
+  }
+
+  p::before {
+      content: '"'
+  }
+
+  p::after {
+      content: '"'
+  }
+
+  span, a {
+      align-self:end;
+      color: #406473;
+  }
+
+  span::before {
+      content: "- "
+  }
+
+  #buttons {
+    display:grid;
+    grid-template-columns: 2fr 1fr;
+  }
+
+  button {
+      background:#406473;
+      color: white;
+      padding:10px;
+      border: 0;
+      font-size:1.2rem;
+      border-radius:0 5px 0 5px;
+      font-weight:bold;
+      margin-top:20px;
+      cursor:pointer;
+      transition: transform 0.2s;
+  }
+  button:hover{
+      transform: scale(1.05);
+  }
+  button:disabled {
+  background:grey;
+  cursor: not-allowed;
+  opacity:0.5;
+}
+</style>
