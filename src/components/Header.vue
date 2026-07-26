d4b5f9043d670580634a9f96dac023d3becc4bd8
Jekath <>
2026-07-26 13:41:53 +0200

    Creating Header-Component

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<script setup>
+    import {ref} from 'vue'
+    const heading = ref("Quote generator")
+</script>
+
+<template>
+  <header>
+      <h1>{{ heading }}</h1>
+  </header>
+</template>
+
+<style scoped>
+  header {
+      color:#CCD6D9;
+      margin-bottom:25px;
+  }
+</style>
