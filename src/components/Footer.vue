35889bfae1c9aea160572517bb86a7506330bef3
Jekath <>
2026-07-26 13:42:16 +0200

    Setting up Footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,31 @@
+<script setup>
+    import {ref} from 'vue'
+    const year = ref("2020")
+    const href = ref("https://scrimba.com")
+    year.value = new Date().getFullYear();
+</script>
+   
+<template>
+  <footer>
+      Created by <a :href> <span>Professor Pickle</span></a> &copy; {{ year }}
+  </footer>
+</template>
+
+<style scoped>
+  footer {
+    position:fixed;
+    margin: 0 auto;
+    bottom:0;
+    text-align:center;
+    color:#CCD6D9;
+    padding-bottom:20px;
+  }
+
+  a {
+    color:#CCD6D9;
+  }
+
+  span{
+      font-family: cursive;
+  }
+</style>
